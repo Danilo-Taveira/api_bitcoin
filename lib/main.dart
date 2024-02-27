42bdde7458614585819bb7d4fda78fe6a51ffd11
Danilo-Taveira <dtnavega@gmail.com>
2024-02-27 12:45:59 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:api_bitcoin/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    )
+  );
+}
